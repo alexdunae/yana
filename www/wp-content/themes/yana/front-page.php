a6fd94c93c5f4c95b8005e2204a96e365558e781
Alex Dunae <alex@dunae.ca>
2013-12-28 16:50:06 -0800

    Proper links in homepage circles

--- www/wp-content/themes/yana/front-page.php
+++ www/wp-content/themes/yana/front-page.php
@@ -4,28 +4,28 @@
   <div class="content">
     <div class="entry entry-0">
       <div class="inner">
-        <h2 class="title"><a href="#todo">Get Support for your Family</a></h2>
+        <h2 class="title"><a href="<?php echo get_permalink( get_page_by_path('get-support') ); ?>">Get Support for your Family</a></h2>
         <p>Find out how Y.A.N.A. can help make things easier for your family.</p>
       </div>
     </div>
 
     <div class="entry entry-1">
       <div class="inner">
-        <h2 class="title"><a href="#todo">Organize a Fundraiser</a></h2>
+        <h2 class="title"><a href="<?php echo get_permalink( get_page_by_path('fundraise') ); ?>">Organize a Fundraiser</a></h2>
         <p>Planning a fundraising event to benefit local families? We can help.</p>
       </div>
     </div>
 
     <div class="entry entry-2">
       <div class="inner">
-        <h2 class="title"><a href="#todo">Volunteer with us</a></h2>
+        <h2 class="title"><a href="<?php echo get_permalink( get_page_by_path('volunteer') ); ?>">Volunteer with us</a></h2>
         <p>Interested in offering your time to Y.A.N.A.? We’d be happy to have your help.</p>
       </div>
     </div>
 
     <div class="entry entry-3">
       <div class="inner">
-        <h2 class="title"><a href="#todo">Donate to YANA</a></h2>
+        <h2 class="title"><a href="<?php echo get_permalink( get_page_by_path('donate') ); ?>">Donate to YANA</a></h2>
         <p>We couldn’t help without your help. Learn about the many ways to give.</p>
       </div>
     </div>
