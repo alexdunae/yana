37ee04d12c555602ead3a37603bfc1738979e252
Alex Dunae <alex@dunae.ca>
2014-01-03 15:13:53 -0800

    Remove periods

--- www/wp-content/themes/yana/front-page.php
+++ www/wp-content/themes/yana/front-page.php
@@ -5,7 +5,7 @@
     <div class="entry entry-0">
       <div class="inner">
         <h2 class="title"><a href="<?php echo get_permalink( get_page_by_path('get-support') ); ?>">Get Support for your Family</a></h2>
-        <p>Find out how Y.A.N.A. can help make things easier for your family.</p>
+        <p>Find out how YANA can help make things easier for your family.</p>
       </div>
     </div>
 
@@ -19,7 +19,7 @@
     <div class="entry entry-2">
       <div class="inner">
         <h2 class="title"><a href="<?php echo get_permalink( get_page_by_path('volunteer') ); ?>">Volunteer with us</a></h2>
-        <p>Interested in offering your time to Y.A.N.A.? We’d be happy to have your help.</p>
+        <p>Interested in offering your time to YANA? We’d be happy to have your help.</p>
       </div>
     </div>
 
