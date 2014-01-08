1849e5e4a2c7db271d3622a1e942f7ed9b2682fe
Alex Dunae <alex@dunae.ca>
2014-01-07 16:27:25 -0800

    Basic 404 page

--- www/wp-content/themes/yana/404.php
+++ www/wp-content/themes/yana/404.php
@@ -1,56 +1,22 @@
-<?php
-/**
- * The template for displaying 404 pages (Not Found).
- *
- * @package YANA
- */
+<?php get_header(); ?>
 
-get_header(); ?>
+<article class="site-body" role="main" id="main">
+   <div class="content">
+   		<?php get_sidebar(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+			<div id="post-error-404" class="error-page">
+				<header class="entry-header">
+					<h1 class="entry-title">Web page not found</h1>
+				</header>
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'yana' ); ?></h1>
-				</header><!-- .page-header -->
+				<div class="entry-content">
+					<p>Unfortunately we couldn't find the page you were looking for.  Sorry.</p>
 
-				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'yana' ); ?></p>
+					<p>Try using the links above the get back on your way.</p>
+				</div>
+			</div>
 
-					<?php get_search_form(); ?>
+	</div>
+</article>
 
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
-
-					<?php if ( yana_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-					<div class="widget widget_categories">
-						<h2 class="widgettitle"><?php _e( 'Most Used Categories', 'yana' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-					<?php endif; ?>
-
-					<?php
-					/* translators: %1$s: smiley */
-					$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'yana' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-					?>
-
-					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php get_footer(); ?>
+<?php get_footer();
