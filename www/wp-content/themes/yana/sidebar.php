e4bd063c655dc29ef70668ced2632719592ea38d
Alex Dunae <alex@dunae.ca>
2014-01-22 15:28:16 -0800

    Sidebar event ads

--- www/wp-content/themes/yana/sidebar.php
+++ www/wp-content/themes/yana/sidebar.php
@@ -49,6 +49,12 @@
 	?>
 
 	<?php if (is_front_page() ): ?>
+	<aside class="sidebar-ad">
+		<a class="frame" href='<?php echo get_permalink( get_page_by_path( 'events' ) ); ?>'>
+			<img src="<?php echo get_template_directory_uri(); ?>/img/ads/sidebar-upcoming-events.jpg" width="306" height="216" alt="Upcoming Events">
+		</a>
+	</aside>
+
 	<aside class="sidebar-facebook-ad">
 		<a class="frame" href='<?php echo YANA\FACEBOOK_URL; ?>'>
 			<span class="inner">
