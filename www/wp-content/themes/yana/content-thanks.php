856948bf90d4be319253b9a769bf72e0c2d7ef68
Alex Dunae <alex@dunae.ca>
2014-02-12 11:08:16 -0800

    Don't truncate 'more' links on thank yous

--- www/wp-content/themes/yana/content-thanks.php
+++ www/wp-content/themes/yana/content-thanks.php
@@ -1,4 +1,7 @@
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
   <img src="<?php echo get_template_directory_uri(); ?>/img/heart.png" width="72" height="64" alt="" class="icon">
-  <?php the_content(); ?>
+  <?php
+    // don't truncate 'more' links on thank yous
+    echo apply_filters('the_content', $post->post_content);
+  ?>
 </div>
