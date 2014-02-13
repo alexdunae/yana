16469d50dcf27188f782afb73ec5a25097abd3b0
Alex Dunae <alex@dunae.ca>
2014-02-12 21:05:25 -0800

    Clear out middleman files

--- Gemfile
+++
@@ -1,6 +0,0 @@
-source "https://rubygems.org/"
-
-gem "middleman", "~> 3.2.0"
-gem "middleman-autoprefixer"
-gem "middleman-deploy"
-gem "addressable"
--- source/javascripts/site.js
+++
@@ -1,7 +0,0 @@
-//= require vendor/console
-//= require vendor/cookies
-//= require vendor/jquery
-//= require vendor/jquery.ba-throttle-debounce
-//= require vendor/jquery.stellar
-//= require vendor/jquery.isonscreen
-//= require yana
