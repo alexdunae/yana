d56e1bd380153ee810226b16f98d79f114aa236d
Alex Dunae <alex@dunae.ca>
2015-12-10 13:19:42 -0800

    Fixes before Dreamhost move

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ROOTDIR="www"
 BASEDIR="www/wp-content/themes/yana"
-USER="yana"
+USER="yanacomoxvalley"
 HOST="yanacomoxvalley.com"
 rsync -rvzt --delete --delete-excluded --executability --omit-dir-times \
       --exclude=.DS_Store \
@@ -9,6 +9,6 @@ rsync -rvzt --delete --delete-excluded --executability --omit-dir-times \
       --exclude=.git \
       --exclude=css/ \
       --exclude=*.coffee \
-      $BASEDIR $USER@$HOST:/home/yana/www/wp-content/themes/
-scp $ROOTDIR/favicon.ico $USER@$HOST:/home/yana/www/
+      $BASEDIR $USER@$HOST:/home/yanacomoxvalley/yanacomoxvalley.com/wp-content/themes/
+scp $ROOTDIR/favicon.ico $USER@$HOST:/home/yanacomoxvalley/yanacomoxvalley.com/
 echo "Finished deploying to $HOST"
